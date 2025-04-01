788ae04c5fd1280f09f0337a7d08f2ab96d145b2
Mortalfrank <s3767319@student.rmit.edu.au>
2025-04-01 15:28:34 +1100

    register,login and home page UI DESIGN

--- 1.1/addStudents.py
+++ 1.1/addStudents.py
@@ -1,6 +1,6 @@
 import boto3
 
-dynamoDB = boto3.resource('dynamodb', 
+dynamoDB = boto3.resource('dynamodb',
                        region_name='us-east-1',
                        endpoint_url='http://localhost:8001') 
 
